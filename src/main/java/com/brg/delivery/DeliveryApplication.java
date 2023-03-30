c7dbc6376ba0d28256cf4b6a17b9d9d2ce7ca684
isaac1102 <hitman1439@naver.com>
2023-03-30 16:26:36 +0900

    fix : 패키지명 변경

--- src/main/java/com/brg/delivery/DeliveryApplication.java
+++ src/main/java/com/brg/delivery/DeliveryApplication.java
@@ -1,4 +1,4 @@
-package com.example.delivery;
+package com.brg.delivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/brg/delivery/DeliveryApplicationTests.java
+++ src/test/java/com/brg/delivery/DeliveryApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.delivery;
+package com.brg.delivery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
