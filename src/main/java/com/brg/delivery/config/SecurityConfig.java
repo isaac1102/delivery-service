32863674bc29563c754046fc481b28b550c328c8
isaac1102 <hitman1439@naver.com>
2023-03-31 12:55:04 +0900

    add : 인증/인가 처리를 위한 JwtFilter 설정(작업중)

--- src/main/java/com/brg/delivery/config/SecurityConfig.java
+++ src/main/java/com/brg/delivery/config/SecurityConfig.java
@@ -1,26 +1,37 @@
 package com.brg.delivery.config;
 
-import org.springframework.context.annotation.Bean;
+import com.brg.delivery.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
-    @Bean
+    private final UserService userService;
+
+    @Value("${jwt.secret}")
+    private final String secretKey;
+
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         return httpSecurity
                 .httpBasic().disable()
                 .csrf().disable()
                 .cors().and()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/**").permitAll()
                 .requestMatchers("/api/v1/users/signup", "/api/v1/users/login").permitAll()
+                .requestMatchers("/api/v1/delivery").authenticated()
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
+                .addFilterBefore(new JwtFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
