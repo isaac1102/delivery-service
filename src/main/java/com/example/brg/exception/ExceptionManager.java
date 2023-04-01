9a036a7c03d86a1e84960b89b316afd53c47d041
isaac1102 <hitman1439@naver.com>
2023-04-02 03:48:37 +0900

    chore : import 내용 수정

--- src/main/java/com/example/brg/exception/ExceptionManager.java
+++ src/main/java/com/example/brg/exception/ExceptionManager.java
@@ -9,7 +9,6 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
 import java.util.ArrayList;
 import java.util.List;
 
