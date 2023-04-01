974ce01cd96549ff3aa6f9c87ff41b5d4cb5bf54
isaac1102 <hitman1439@naver.com>
2023-04-02 03:46:18 +0900

    add : 테스트데이터 생성

--- src/main/java/com/example/brg/AppRunner.java
+++ src/main/java/com/example/brg/AppRunner.java
@@ -7,6 +7,7 @@ import com.example.brg.domain.user.model.User;
 import com.example.brg.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 
@@ -15,24 +16,39 @@ import java.time.LocalDateTime;
 public class AppRunner implements CommandLineRunner {
     private final UserRepository userRepository;
     private final DeliveryRepository deliveryRepository;
+    private final BCryptPasswordEncoder encoder;
 
     @Override
     public void run(String... args) {
-        userRepository.save(new User("test1", "aaaaaaaaaaB.1", "Mr.Kim"));
-        userRepository.save(new User("test2", "aaaaaaaaaaB.1", "Mr.Park"));
-        userRepository.save(new User("test3", "aaaaaaaaaaB.1", "Mr.Lee"));
+        userRepository.save(new User("test1", encoder.encode("aaaaaaaaaaB.1"), "Mr.Kim"));
+        userRepository.save(new User("test2", encoder.encode("aaaaaaaaaaB.1"), "Mr.Park"));
+        userRepository.save(new User("test3", encoder.encode("aaaaaaaaaaB.1"), "Mr.Lee"));
 
         User test1 = userRepository.findByUserId("test1").get();
         User test2 = userRepository.findByUserId("test2").get();
         User test3 = userRepository.findByUserId("test3").get();
+        int deliveryCount = 10;
 
-        for(int i = 1; i <= 10; i++){
-            deliveryRepository.save(new Delivery(test1, LocalDateTime.of(2023, 1, i, 0, 0),
-                    LocalDateTime.of(2023, 1, i, 0, 0), "samsung-dong", DeliveryStatus.ARRIVED));
-            deliveryRepository.save(new Delivery(test2, LocalDateTime.of(2023, 2, i, 0, 0),
-                    LocalDateTime.of(2023, 2, i, 0, 0), "seocho-dong", DeliveryStatus.ARRIVED));
-            deliveryRepository.save(new Delivery(test3, LocalDateTime.of(2023, 3, i, 0, 0),
-                    LocalDateTime.of(2023, 3, i, 0, 0), "yeoksam-dong", DeliveryStatus.ARRIVED));
+        for (int i = 1; i <= deliveryCount; i++) {
+            deliveryRepository.save(
+                    new Delivery(test1,
+                            LocalDateTime.of(2023, 1, i, 0, 0),
+                            LocalDateTime.of(2023, 1, i, 0, 0),
+                            "samsung-dong", DeliveryStatus.ARRIVED));
+
+            deliveryRepository.save(
+                    new Delivery(test2,
+                            LocalDateTime.of(2023, 2, i, 0, 0),
+                            LocalDateTime.of(2023, 2, i, 0, 0),
+                            "seocho-dong",
+                            DeliveryStatus.ARRIVED));
+
+            deliveryRepository.save(
+                    new Delivery(test3,
+                            LocalDateTime.of(2023, 3, i, 0, 0),
+                            LocalDateTime.of(2023, 3, i, 0, 0),
+                            "yeoksam-dong",
+                            DeliveryStatus.ARRIVED));
         }
     }
 }
