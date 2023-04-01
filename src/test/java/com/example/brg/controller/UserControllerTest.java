f21c112e81ce19cdd219417d0ce3fc14253e6eb8
isaac1102 <hitman1439@naver.com>
2023-04-02 03:53:27 +0900

    add : controller 테스트 작성중

--- src/test/java/com/example/brg/controller/UserControllerTest.java
+++ src/test/java/com/example/brg/controller/UserControllerTest.java
@@ -1,8 +1,9 @@
 package com.example.brg.controller;
 
 import com.example.brg.config.SecurityConfig;
-import com.example.brg.domain.user.request.LoginRequest;
-import com.example.brg.domain.user.request.SignUpRequest;
+import com.example.brg.domain.user.controller.UserController;
+import com.example.brg.domain.user.controller.request.LoginRequest;
+import com.example.brg.domain.user.controller.request.SignUpRequest;
 import com.example.brg.domain.user.exception.UserException;
 import com.example.brg.domain.user.exception.UserErrorCode;
 import com.example.brg.domain.user.service.UserService;
@@ -20,10 +21,9 @@ import static org.springframework.security.test.web.servlet.request.SecurityMock
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest
+@WebMvcTest(controllers = UserController.class)
 class UserControllerTest {
 
     @Autowired
@@ -41,34 +41,80 @@ class UserControllerTest {
     @Test
     @DisplayName("회원가입 성공")
     @WithMockUser
-    void join_success() throws Exception {
-        String userId = "user1";
-        String password = "ajajajajajA.1";
-        String userName = "user name";
+    void signup_success() throws Exception {
+        SignUpRequest signUpRequest = new SignUpRequest("test10", "aaaaaaaaaaB.1", "testo");
 
         mockMvc.perform(post("/api/v1/users/signup")
-                        .with(csrf())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsBytes(new SignUpRequest(userId, password, userName))))
-                .andDo(print())
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(signUpRequest)))
                 .andExpect(status().isOk());
     }
 
     @Test
-    @DisplayName("회원가입 실패")
+    @DisplayName("비밀번호 글자수 미충족으로 인한 valid 통과 실패")
     @WithMockUser
-    void join_fail() throws Exception {
-        String userId = "user1";
-        String password = "ajajajajajA.1";
-        String userName = "user name";
+    void signup_fail_invalid_password() throws Exception {
+        SignUpRequest signUpRequest = new SignUpRequest("test1", "aabbbbB.1", "testo");
+
+        mockMvc.perform(post("/api/v1/users/signup")
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(signUpRequest)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("비밀번호 대소문자 규칙 미충족으로 인한 valid 통과 실패")
+    @WithMockUser
+    void signup_fail_invalid_password_character() throws Exception {
+        SignUpRequest signUpRequest = new SignUpRequest("test1", "aabbbba.1", "testo");
+
+        mockMvc.perform(post("/api/v1/users/signup")
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(signUpRequest)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("비밀번호 특수문자 미포함으로 인한 valid 통과 실패")
+    @WithMockUser
+    void signup_fail_invalid_password_special_character() throws Exception {
+        SignUpRequest signUpRequest = new SignUpRequest("test1", "aaaaaaaaaB31", "testo");
+
+        mockMvc.perform(post("/api/v1/users/signup")
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(signUpRequest)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("가입자 id 길이 valid 통과 실패")
+    @WithMockUser
+    void signup_fail_by_name() throws Exception {
+        SignUpRequest signUpRequest = new SignUpRequest("test", "aaaaaaaaaB31", "testo");
+
+        mockMvc.perform(post("/api/v1/users/signup")
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(signUpRequest)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("기존 가입자는 회원가입 실패")
+    @WithMockUser
+    void signup_fail() throws Exception {
+        SignUpRequest signUpRequest = new SignUpRequest("test10", "aaaaaaaaaaB.1", "testo");
 
         Mockito.doThrow(new UserException(UserErrorCode.ALREADY_EXISTING_ACCOUNT, "")).when(userService).createUser(any(), any(), any());
 
         mockMvc.perform(post("/api/v1/users/signup")
-                        .with(csrf())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsBytes(new SignUpRequest(userId, password, userName))))
-                .andDo(print())
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(signUpRequest)))
                 .andExpect(status().isConflict());
     }
 
@@ -76,27 +122,71 @@ class UserControllerTest {
     @DisplayName("로그인 성공")
     @WithMockUser
     void login_success() throws Exception {
-        when(userService.login("aaaaaa", "bbbbbbb")).thenReturn("token");
+        String userId = "test1";
+        String password = "aaaaaaaaaaaB.1";
 
-         mockMvc.perform(post("/api/v1/users/login")
-                        .with(csrf())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsBytes(new LoginRequest("aaaaaa","bbbbbbb"))))
-                .andDo(print())
+        LoginRequest loginRequest = new LoginRequest(userId, password);
+
+        when(userService.login(userId, password)).thenReturn("token");
+
+        mockMvc.perform(post("/api/v1/users/login")
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(loginRequest)))
                 .andExpect(status().isOk());
     }
 
     @Test
-    @DisplayName("로그인 실패")
+    @DisplayName("비밀번호가 일치하지 않는 경우 실패")
     @WithMockUser
     void login_fail() throws Exception {
-        when(userService.login("aa", "bb")).thenThrow(new UserException(UserErrorCode.INVALID_PASSWORD, ""));
+        String userId = "test1";
+        String password = "aaaaaaaaaaaB.1";
+
+        LoginRequest loginRequest = new LoginRequest(userId, password);
+
+        when(userService.login(userId, password)).thenThrow(new UserException(UserErrorCode.INVALID_PASSWORD, ""));
 
         mockMvc.perform(post("/api/v1/users/login")
-                        .with(csrf())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsBytes(new LoginRequest("aa","bb"))))
-                .andDo(print())
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(loginRequest)))
                 .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @DisplayName("비밀번호가 blank인 경우 예외발생")
+    @WithMockUser
+    void login_fail_password_is_blank() throws Exception {
+        String userId = "test1";
+        String password = "";
+
+        LoginRequest loginRequest = new LoginRequest(userId, password);
+
+        when(userService.login(userId, password)).thenThrow(new UserException(UserErrorCode.INVALID_PASSWORD, ""));
+
+        mockMvc.perform(post("/api/v1/users/login")
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(loginRequest)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("유저아이디가 blank인 경우 예외발생")
+    @WithMockUser
+    void login_fail_userId_is_blank() throws Exception {
+        String userId = "";
+        String password = "aaaaaaaaaaB.1";
+
+        LoginRequest loginRequest = new LoginRequest(userId, password);
+
+        when(userService.login(userId, password)).thenThrow(new UserException(UserErrorCode.INVALID_PASSWORD, ""));
+
+        mockMvc.perform(post("/api/v1/users/login")
+                .with(csrf())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(loginRequest)))
+                .andExpect(status().isBadRequest());
     }
 }
