747d6cbe4d2d448b617c322a87c7df9beecc476f
isaac1102 <hitman1439@naver.com>
2023-04-02 03:50:18 +0900

    add : Authorization 헤더값으로부터 검증 및 userId 추출 로직 추가

--- src/main/java/com/example/brg/config/JwtFilter.java
+++ src/main/java/com/example/brg/config/JwtFilter.java
@@ -1,12 +1,14 @@
 package com.example.brg.config;
 
-import com.example.brg.domain.user.service.UserService;
+import com.example.brg.utils.JwtUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -16,18 +18,35 @@ import java.io.IOException;
 import java.util.List;
 
 @RequiredArgsConstructor
+@Slf4j
 public class JwtFilter extends OncePerRequestFilter {
-    private final UserService userService;
     @Value("${jwt.secret}")
     private final String secretKey;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String userName = "";
+        String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
+        log.info("authorization : {}", authorization);
+
+        if (authorization == null || !authorization.startsWith("Bearer")) {
+            throw new RuntimeException("authorization is null이거나 잘못되었습니다.");
+        }
+
+        String token = authorization.split(" ")[1];
+
+        // jwt 만료여부 확인
+        if (JwtUtil.isExpired(token, secretKey)) {
+            throw new RuntimeException("Token이 만료되었습니다.");
+        }
+
+        String userId = JwtUtil.getUserId(token, secretKey);
+        log.info("userId : {}", userId);
+
 
         // 권한 부여
         UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken(userName, null, List.of(new SimpleGrantedAuthority("USER")));
+                new UsernamePasswordAuthenticationToken(
+                        userId, null, List.of(new SimpleGrantedAuthority("USER")));
         authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
         SecurityContextHolder.getContext().setAuthentication(authenticationToken);
         filterChain.doFilter(request, response);
