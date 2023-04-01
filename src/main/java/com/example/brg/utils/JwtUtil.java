b0e4c9cd7b10956965856d75417db5864f22814c
isaac1102 <hitman1439@naver.com>
2023-04-02 03:50:53 +0900

    add : jwt 만료 검증 메서드 추가

--- src/main/java/com/example/brg/utils/JwtUtil.java
+++ src/main/java/com/example/brg/utils/JwtUtil.java
@@ -1,13 +1,27 @@
 package com.example.brg.utils;
 
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-
 import java.util.Date;
 
-public class JwtUtil {
-    public static String createToken(String userId, String key, long expireTimeMs){
+public final class JwtUtil {
+    public static final Long expiredTimeMs = 1000 * 60 * 60L;
+
+    private JwtUtil() { }
+
+    public static String getUserId(String token, String secretKey) {
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody()
+                .get("userId", String.class);
+    }
+
+    public static String createToken(String userId,
+                                     String key,
+                                     long expireTimeMs){
         Claims claims = Jwts.claims();
         claims.put("userId", userId);
 
@@ -17,5 +31,13 @@ public class JwtUtil {
                 .setExpiration(new Date(System.currentTimeMillis() + expireTimeMs))
                 .signWith(SignatureAlgorithm.HS256, key)
                 .compact();
+    }
+
+    public static boolean isExpired(String token, String secretKey) throws ExpiredJwtException{
+        return Jwts.parser().setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody()
+                .getExpiration()
+                .before(new Date());
     }
 }
