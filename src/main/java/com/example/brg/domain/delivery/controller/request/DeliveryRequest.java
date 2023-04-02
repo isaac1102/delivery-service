62263b0423fcb0c7947d92b4d5bcc9e94eaadd4b
isaac1102 <hitman1439@naver.com>
2023-04-02 23:27:11 +0900

    update : 불필요 코드 제거

--- src/main/java/com/example/brg/domain/delivery/controller/request/DeliveryRequest.java
+++ src/main/java/com/example/brg/domain/delivery/controller/request/DeliveryRequest.java
@@ -1,7 +1,5 @@
 package com.example.brg.domain.delivery.controller.request;
 
-import com.example.brg.domain.delivery.exception.DeliveryErrorCode;
-import com.example.brg.domain.delivery.exception.DeliveryException;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 import java.time.LocalDate;
@@ -17,19 +15,5 @@ public class DeliveryRequest {
     public DeliveryRequest(LocalDate startDate, LocalDate endDate){
         this.startDate = startDate;
         this.endDate = endDate;
-
-        isValidDate();
-    }
-
-    public void isValidDate() {
-        if (endDate.isBefore(startDate)) {
-            throw new DeliveryException(DeliveryErrorCode.INVALID_DATE_RANGE,
-                    DeliveryErrorCode.INVALID_DATE_RANGE.getMessage());
-        }
-
-        if (startDate.plusDays(3).isBefore(endDate)) {
-            throw new DeliveryException(DeliveryErrorCode.EXEED_MAX_DATE_RANGE,
-                    DeliveryErrorCode.EXEED_MAX_DATE_RANGE.getMessage());
-        }
     }
 }
